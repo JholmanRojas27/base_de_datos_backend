3d4db90d9c91eb378f941c0efd231ed458cdd098
Jholman Duvan Rojas Bautista <ing_jholmanrojas@hotmail.com>
2024-10-12 15:03:27 -0500

    modified:   app/controllers/cliente_controllers.py

--- app/controllers/cliente_controllers.py
+++ app/controllers/cliente_controllers.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request,jsonify
 from flask_restx import Namespace, Resource,fields
 from app.services.cliente_services import ClienteService
 
@@ -18,3 +18,24 @@ class ClienteResource(Resource):
     def post(self):
         data = request.get_json()
         cliente = ClienteService.create_cliente(data['cli_nombre'],data['cli_direccion'],data['cli_telefono'],data)
+        return jsonify({'message': 'Cliente creado satisfactoriamente', 'cliente':cliente.cli_id})
+    
+    @clienet_ns.doc('get_clientes')
+    def get(self):
+        cliente = ClienteService.get_all_cliente()
+        return jsonify({'clientes':[clientes.cli_id for clientes in cliente]})
+    
+    @clienet_ns.route('<cli_id>')
+    @clienet_ns.param('cli_id', 'el ID del cliente')
+    class ClienteDetailResource(Resource):
+        @clienet_ns.doc('delete_cliente')
+        def delet(self,cli_id):
+            ClienteService.delete_cliente(cli_id)
+            return jsonify({'mesage': 'El cliente se borró satisfactoriamente'})
+    
+    @clienet_ns.doc('update_cliente')
+    @clienet_ns.expect(cliente_model, validate = True)
+    def put(self,cli_id):
+        new_data = request.get_json()
+        ClienteService.update_cliente(cli_id,new_data)
+        return jsonify({'message': 'El cliente fue actualizado satisfactoriamente'})
